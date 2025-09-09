69daf041807a7451e2ad6f2f8385c2a93bbdb3b9
HeyUKonrad <konrad.m.kappmeyer@gmail.com>
2025-09-09 09:59:25 +0200

    init commit

---
+++ lib/goblin_trader.ex
@@ -0,0 +1,18 @@
+defmodule GoblinTrader do
+  @moduledoc """
+  Documentation for `GoblinTrader`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> GoblinTrader.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/goblin_trader_test.exs
@@ -0,0 +1,8 @@
+defmodule GoblinTraderTest do
+  use ExUnit.Case
+  doctest GoblinTrader
+
+  test "greets the world" do
+    assert GoblinTrader.hello() == :world
+  end
+end
